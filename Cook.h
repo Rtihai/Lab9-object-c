12998c3a0d132726d7184262d0bc60ce2816514e
Roman Tihai <46790893+Rtihai@users.noreply.github.com>
2019-08-08 23:12:58 -0700

    Create Cook.h

---
+++ Cook.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import "FoodTruck.h"
+
+@interface Cook : NSObject <FoodTruckDelegate>
+- (double) foodTruck: (FoodTruck *) truck priceForFood:(NSString *)food;
+
+@end
